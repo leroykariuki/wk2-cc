92280212795452d9195b070e29d355adfae60616
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-03 12:43:43 +0300

    review.py

---
+++ review.py
@@ -0,0 +1,12 @@
+# review.py
+class Review:
+    all_reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+        Review.all_reviews.append(self)
+
+    def rating(self):
+        return self.rating
